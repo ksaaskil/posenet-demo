394e652db059b939dd0191790f88fda6c560f394
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-29 16:46:13 +0200

    First commit.

---
+++ camera.js
@@ -0,0 +1,236 @@
+/**
+ * @license
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+import * as posenet from "@tensorflow-models/posenet";
+// import dat from 'dat.gui';
+import Stats from "stats.js";
+
+import {
+  drawBoundingBox,
+  drawKeypoints,
+  drawSkeleton,
+  isMobile,
+  toggleLoadingUI,
+  tryResNetButtonName,
+  tryResNetButtonText,
+  updateTryResNetButtonDatGuiCss,
+} from "./demo_util";
+
+const videoWidth = 600;
+const videoHeight = 500;
+const stats = new Stats();
+
+/**
+ * Loads a the camera to be used in the demo
+ *
+ */
+async function setupCamera() {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    throw new Error(
+      "Browser API navigator.mediaDevices.getUserMedia not available"
+    );
+  }
+
+  const video = document.getElementById("video");
+  video.width = videoWidth;
+  video.height = videoHeight;
+
+  const mobile = isMobile();
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      facingMode: "user",
+      width: mobile ? undefined : videoWidth,
+      height: mobile ? undefined : videoHeight,
+    },
+  });
+  video.srcObject = stream;
+
+  return new Promise(resolve => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+
+async function loadVideo() {
+  const video = await setupCamera();
+  video.play();
+
+  return video;
+}
+
+const defaultQuantBytes = 2;
+
+const defaultMobileNetMultiplier = isMobile() ? 0.5 : 0.75;
+const defaultMobileNetStride = 16;
+const defaultMobileNetInputResolution = 513;
+
+const defaultResNetMultiplier = 1.0;
+const defaultResNetStride = 32;
+const defaultResNetInputResolution = 250;
+
+const guiState = {
+  algorithm: "single-pose",
+  input: {
+    architecture: "MobileNetV1",
+    outputStride: defaultMobileNetStride,
+    inputResolution: defaultMobileNetInputResolution,
+    multiplier: defaultMobileNetMultiplier,
+    quantBytes: defaultQuantBytes,
+  },
+  singlePoseDetection: {
+    minPoseConfidence: 0.1,
+    minPartConfidence: 0.5,
+  },
+  multiPoseDetection: {
+    maxPoseDetections: 5,
+    minPoseConfidence: 0.15,
+    minPartConfidence: 0.1,
+    nmsRadius: 30.0,
+  },
+  output: {
+    showVideo: true,
+    showSkeleton: true,
+    showPoints: true,
+    showBoundingBox: false,
+  },
+  net: null,
+};
+
+/**
+ * Sets up a frames per second panel on the top-left of the window
+ */
+function setupFPS() {
+  stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.getElementById("main").appendChild(stats.dom);
+}
+
+/**
+ * Feeds an image to posenet to estimate poses - this is where the magic
+ * happens. This function loops with a requestAnimationFrame method.
+ */
+function detectPoseInRealTimeWithoutGUI(video, net) {
+  const canvas = document.getElementById("output");
+  const ctx = canvas.getContext("2d");
+
+  // since images are being fed from a webcam, we want to feed in the
+  // original image and then just flip the keypoints' x coordinates. If instead
+  // we flip the image, then correcting left-right keypoint pairs requires a
+  // permutation on all the keypoints.
+  const flipPoseHorizontal = true;
+
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+
+  async function poseDetectionFrame() {
+    // Begin monitoring code for frames per second
+    stats.begin();
+
+    let poses = [];
+    let minPoseConfidence;
+    let minPartConfidence;
+
+    const pose = await net.estimatePoses(video, {
+      flipHorizontal: flipPoseHorizontal,
+      decodingMethod: "single-person",
+    });
+    poses = poses.concat(pose);
+    const guiMinPoseConfidence = guiState.singlePoseDetection.minPoseConfidence;
+    const guiMinPartConfidence = guiState.singlePoseDetection.minPartConfidence;
+    minPoseConfidence = +guiMinPoseConfidence;
+    minPartConfidence = +guiMinPartConfidence;
+
+    ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+    const showVideo = true;
+    if (showVideo) {
+      ctx.save();
+      ctx.scale(-1, 1);
+      ctx.translate(-videoWidth, 0);
+      ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+      ctx.restore();
+    }
+
+    const showPoints = true;
+    const showSkeleton = true;
+    const showBoundingBox = true;
+
+    // For each pose (i.e. person) detected in an image, loop through the poses
+    // and draw the resulting skeleton and keypoints if over certain confidence
+    // scores
+    poses.forEach(({ score, keypoints }) => {
+      if (score >= minPoseConfidence) {
+        if (showPoints) {
+          drawKeypoints(keypoints, minPartConfidence, ctx);
+        }
+        if (showSkeleton) {
+          drawSkeleton(keypoints, minPartConfidence, ctx);
+        }
+        if (showBoundingBox) {
+          drawBoundingBox(keypoints, ctx);
+        }
+      }
+    });
+
+    // End monitoring code for frames per second
+    stats.end();
+
+    requestAnimationFrame(poseDetectionFrame);
+  }
+
+  poseDetectionFrame();
+}
+
+/**
+ * Kicks off the demo by loading the posenet model, finding and loading
+ * available camera devices, and setting off the detectPoseInRealTime function.
+ */
+export async function bindPage() {
+  toggleLoadingUI(true);
+  const net = await posenet.load({
+    architecture: guiState.input.architecture,
+    outputStride: guiState.input.outputStride,
+    inputResolution: guiState.input.inputResolution,
+    multiplier: guiState.input.multiplier,
+    quantBytes: guiState.input.quantBytes,
+  });
+  toggleLoadingUI(false);
+
+  let video;
+
+  try {
+    video = await loadVideo();
+  } catch (e) {
+    let info = document.getElementById("info");
+    info.textContent =
+      "this browser does not support video capture," +
+      "or this device does not have a camera";
+    info.style.display = "block";
+    throw e;
+  }
+
+  // setupGui([], net);
+  setupFPS();
+  detectPoseInRealTimeWithoutGUI(video, net);
+}
+
+navigator.getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
+// kick off the demo
+bindPage();
